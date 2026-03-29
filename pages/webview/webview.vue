d52755d3bf73f30474bdab471fe91f66a8f7f069
matthewlovechen-afk <matthewlovechen@gmail.com>
2026-03-30 00:10:09 +0800

    Add files via upload

---
+++ pages/webview/webview.vue
@@ -0,0 +1,16 @@
+<template>
+  <web-view :src="url"></web-view>
+</template>
+
+<script>
+export default {
+  onLoad(options) {
+    this.url = decodeURIComponent(options.url);
+  },
+  data() {
+    return {
+      url: ""
+    };
+  }
+};
+</script>
