913d30945d729483944f11c576ff6c58207fcbe5
1Catherine-ctrl <13569026856@163.com>
2026-04-17 18:57:49 +0800

    Update index.js

--- uniCloud-alipay/cloudfunctions/user-register/index.js
+++ uniCloud-alipay/cloudfunctions/user-register/index.js
@@ -15,11 +15,21 @@ exports.main = async (event, context) => {
 	const res = await collection.add({
 		username,
 		password, // 生产环境中应使用加密算法处理密码
+		nickname: username,  // 默认昵称等于用户名
 		create_time: Date.now()
 	});
 	
 	if (res.id) {
-		return { code: 200, message: '注册成功' };
+		// 注册成功后返回用户信息
+		return { 
+			code: 200, 
+			message: '注册成功',
+			userInfo: {
+				_id: res.id,
+				username: username,
+				nickname: username
+			}
+		};
 	} else {
 		return { code: 500, message: '注册失败' };
 	}
